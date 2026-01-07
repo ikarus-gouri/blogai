f33f208895a986585acbd5bcf8508aebe75034d9
Pratham <pratham.gupta@ikarusdelta.com>
2026-01-07 11:22:32 +0530

    classifier using transformers and cosine diff

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from classifier import TextClassifier
 
 if __name__=="__main__":
-    with open("blog.txt", "r", encoding="utf-8") as f:
-        blog_txt = f.read()
-
-    classifier = TextClassifier(threshold=0.4)
-    categories = classifier.classify(blog_txt)
-
-    print("🔹 Predicted Categories:")
+    
+    with open("blog.txt", "r", encoding="utf-8")as f:
+        blog_txt= f.read()
+    classifier=TextClassifier()
+    categories= classifier.classify(blog_txt)
     print(categories)
